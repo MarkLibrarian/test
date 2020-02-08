34b84fabab40a7196e5351fafcc4b267221590f9
Maggie Wang <maggierettenberger@gmail.com>
2020-02-08 10:39:48 +0000

    Add new scene and button components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import './App.css';
+import Scene from './Scene'
+import AddSceneButton from './AddSceneButton';
 
-function App() {
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      scenes:[]
+    }
+  }
+  addNewScene=(e)=>{
+    console.log('Clicked')
+    this.setState((state, props)=>{
+      const newScene={title: "My first scene"}
+      state.scenes.push(newScene)
+      return {
+        scenes:this.state.scenes
+      }
+    })
+  }
+  render(){
+    const scenes = this.state.scenes.map((scene, i) => <Scene key={i}/>)
   return (
     <div className="App">
       <header className="App-header">
+        {scenes}
+        <AddSceneButton addNewScene = {this.addNewScene}/>
+        {/* <p>{this.state.scenes.length}</p> */}
       </header>
     </div>
   );
 }
-
+}
 export default App;
---
+++ src/Scene.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Scene.css'
+
+export default class Scene extends Component {
+    render() {
+        return (
+            <div className="scene">
+                Hello Scene!
+            </div>
+        )
+    }
+}
