301358702dc93bf3fec2848c108ccaf4e7fd2424
MarkLibrarian <markrsutcliffe@gmail.com>
2020-02-10 23:46:47 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./App.css";
+import Scene from "./Scene";
+import AddSceneButton from "./AddSceneButton";
+import RemoveAllScenesButton from "./RemoveAllScenesButton";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scenes: []
+    };
+  }
+  addNewScene = e => {
+    console.log("Clicked");
+    this.setState((state, props) => {
+      const newScene = { title: "My first scene" };
+      state.scenes.push(newScene);
+      return {
+        scenes: this.state.scenes
+      };
+    });
+  };
+  removeAllScenes = e => {
+   
+this.setState({scenes: []});
+
+  };
+  render() {
+    const scenes = this.state.scenes.map((scene, i) => <Scene key={i} />);
+    return (
+      <div className="App">
+        <header className="App-header">
+          {scenes}
+          <AddSceneButton addNewScene={this.addNewScene} />
+          {/* <p>{this.state.scenes.length}</p> */}
+          <RemoveAllScenesButton removeAllScenes={this.removeAllScenes} />
+        </header>
+      </div>
+    );
+  }
+}
+export default App;
