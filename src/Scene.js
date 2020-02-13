592935185aef4ff689550ca9a35fb46ef0c5f9ec
rich <r.evans.1@elsevier.com>
2020-02-13 12:50:29 +0000

    Fix location of Remove All Scenes component

--- src/Scene.js
+++ src/Scene.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react';
-import './Scene.css'
+import React, { Component } from "react";
+import "./Scene.css";
 
 export default class Scene extends Component {
     render() {
         return (
             <div className="scene">
                 Hello Scene!
+                <div>
+                    <textarea defaultValue="Enter the story for your scene here"/>
+                </div>
             </div>
-        )
+        );
     }
 }
