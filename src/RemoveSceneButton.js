f8826f19118a65f1b82a42c94f31f131f377315e
Markrsutcliffe <Markrsutcliffe@gmail.com>
2020-02-20 19:46:57 +0000

    Remove scene button added

---
+++ src/RemoveSceneButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function removeScene(props) {
+    return (
+        <div>
+            <button onClick={props.removeScene}>Remove scene</button>
+        </div>
+    )
+}
