69455e56a502326d1bee3fc0cf5ddb2cc75b1bd8
Markrsutcliffe <Markrsutcliffe@gmail.com>
2020-02-23 21:43:18 +0000

    Functionality added to RemoveSceneButton component

--- src/pages/story/edit/Scene.js
+++ src/pages/story/edit/Scene.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./Scene.css";
 import Image from "./Image";
 import StoryText from "./StoryText";
-
+import RemoveSceneButton from "./RemoveSceneButton.js";
 export default class Scene extends Component {
   constructor(props) {
     super(props);
@@ -18,7 +18,7 @@ export default class Scene extends Component {
       <div className="Scene">
         <Image scene={scene} />
         <StoryText scene={scene} />
-        <button onClick={this.handleRemove}>Remove scene</button>
+        <RemoveSceneButton handleRemove={this.handleRemove} />
       </div>
     );
   }
