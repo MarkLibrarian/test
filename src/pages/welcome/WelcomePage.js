ed941ff8c80b0274b38aa30e0160cc9004a0787f
rich <r.evans.1@elsevier.com>
2020-02-26 15:11:29 +0000

    Basic A-Frame integration

--- src/pages/welcome/WelcomePage.js
+++ src/pages/welcome/WelcomePage.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './WelcomePage.css';
+import {Link} from "react-router-dom";
 
 class WelcomePage extends React.Component {
     render() {
         return (
             <div className="WelcomePage">
                 <h1>Welcome</h1>
+                <ul>
+                    <li><Link to="/story/edit">Edit Story</Link></li>
+                    <li><Link to="/story/view">View Story</Link></li>
+                </ul>
             </div>
         );
     }
--- src/pages/welcome/WelcomePage.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {render} from '@testing-library/react';
-import WelcomePage from './WelcomePage';
-
-test('displays a welcoming message', () => {
-    const {getByText} = render(<WelcomePage/>);
-    const welcomeMessage = getByText(/Welcome/i);
-    expect(welcomeMessage).toBeInTheDocument();
-});
