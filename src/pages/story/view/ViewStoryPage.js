f815993b53308d5a153f1a6f0ebfef52181c9905
Markrsutcliffe <Markrsutcliffe@gmail.com>
2020-02-26 21:33:01 +0000

    Link traversal in A-Frame added

--- src/pages/story/view/ViewStoryPage.js
+++ src/pages/story/view/ViewStoryPage.js
@@ -1,20 +1,43 @@
-import React from 'react';
-import './ViewStoryPage.css';
-
+import React from "react";
+import "./ViewStoryPage.css";
 class ViewStoryPage extends React.Component {
-    render() {
-        return (
-            <div className="ViewStoryPage">
-                <a-scene>
-                    <a-sky src="/default-image-360.jpg" rotation="0 -130 0"/>
-                    <a-text value="Queen Street, Oxford"
-                            width="6"
-                            position="-2.5 0.25 -1.5"
-                            rotation="0 15 0"/>
-                </a-scene>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="ViewStoryPage">
+        <a-scene
+          raycaster="far: 100; objects: [link];"
+          cursor="rayOrigin: mouse"
+        >
+          <a-sky src="/default-image-360.jpg" rotation="0 -130 0" />
+
+          <a-link
+            href="/WelcomePage"
+            title="Exit to home page"
+            position="-14 5.0 -8.0"
+            image=""
+            geometry=""
+            material=""
+          ></a-link>
+          <a-link
+            href="/WelcomePage"
+            title="Go to scene 1"
+            position="-14 0.5 -8.0"
+            image=""
+            geometry=""
+            material=""
+          ></a-link>
+          <a-link
+            href="/WelcomePage"
+            title="Go to scene 2"
+            position="-14 -2.5 -8.0"
+            image=""
+            geometry=""
+            material=""
+          ></a-link>
+        </a-scene>
+      </div>
+    );
+  }
 }
 
 export default ViewStoryPage;
