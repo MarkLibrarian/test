fe5872ac5948b71d31af45423334066a835a15b0
Maggie Wang <maggierettenberger@gmail.com>
2020-02-29 11:15:32 +0000

    Lifted state up into app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React from 'react';
 import {Route, Switch} from "react-router";
 import {BrowserRouter as Router} from "react-router-dom";
 import EditStoryPage from "./pages/story/edit/EditStoryPage";
 import ViewStoryPage from "./pages/story/view/ViewStoryPage";
 import WelcomePage from "./pages/welcome/WelcomePage";
+import React, { Component } from 'react';
+import { defaultStory } from "./model";
 
-export default function () {
-    return (
-        <Router>
+export default class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            story: defaultStory()
+        };
+    }
+    onStoryChange =(story) => {
+        this.setState({
+            ...this.state,
+            story
+        });
+        console.log(story);
+        console.log(this.state);
+    }
+    render() {
+        return (
+            <Router>
             <Switch>
                 <Route path='/story/edit'>
-                    <EditStoryPage/>
+                    <EditStoryPage 
+                        story={this.state.story}
+                        onStoryChange={this.onStoryChange}/>
                 </Route>
                 <Route path='/story/view'>
                     <ViewStoryPage/>
@@ -20,5 +38,8 @@ export default function () {
                 </Route>
             </Switch>
         </Router>
-    );
+        )
+    }
 }
+
+
