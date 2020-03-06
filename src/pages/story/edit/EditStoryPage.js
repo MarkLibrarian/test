d6d78c36b3ef1c2ea7b9d0f98f90e9164b8a0dd9
rich <r.evans.1@elsevier.com>
2020-03-06 13:13:45 +0000

    Use UUI per the docs 'cos it was moaning

--- src/pages/story/edit/EditStoryPage.js
+++ src/pages/story/edit/EditStoryPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import uuid from "uuid/v4";
+import { v4 as uuid } from "uuid";
 import "./EditStoryPage.css";
 import Scene from "./Scene";
 import AddSceneButton from "./AddSceneButton";
